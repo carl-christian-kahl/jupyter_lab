63d34cc3fc57879387c870a67a95bdb6b78c2ed8
c.kahl <carl.christian.kahl@googlemail.com>
2020-06-19 23:31:41 +0100

    update
    Signed-off-by: c.kahl <carl.christian.kahl@googlemail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyter_lab',
+    version='0.1.0',
+    packages=find_packages(),
+    description = 'Data analytics',
+    url = 'jupyter_lab@bitbucket.org/quant_torch/quant_torch.git',
+    author_email = 'carl.christian.kahl@googlemail.com',
+    license = 'unlicense',
+    zip_safe=False
+)
