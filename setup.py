92d2dad0ffb94eb28c026a483f7afa719a4b4206
c.kahl <carl.christian.kahl@googlemail.com>
2020-07-29 10:27:26 +0200

    ck

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     description = 'Data analytics',
-    url = 'jupyter_lab@bitbucket.org/quant_torch/quant_torch.git',
     author_email = 'carl.christian.kahl@googlemail.com',
     license = 'unlicense',
     zip_safe=False
